574e6b3841fee0ef9521064d7be39b0d174b63c2
dm_kolomiets <dp.caspik@gmail.com>
2017-10-10 16:25:42 +0300

    Add project files.

---
+++ Recieve/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using Serilog;
+
+namespace Recieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration().MinimumLevel.Verbose().WriteTo.RollingFile(@"E:\C#\Приват\RabbitMQ\Send\Logs").WriteTo.ColoredConsole().CreateLogger();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            logger.Debug("Created new factory {factory}", factory.HostName);
+            using (var connection = factory.CreateConnection())
+            {
+                logger.Debug("Created connection {connection}", connection.ToString());
+                using (var channel = connection.CreateModel())
+                {
+                    string queyeName = "first";
+                    channel.QueueDeclare(queyeName, false, false, false, null);
+                    logger.Debug("Created queye {queye}", queyeName);
+
+             
+                    var consumer = new EventingBasicConsumer(channel);
+                    logger.Debug("Created new consumer {consumer}", consumer.ToString());
+                    consumer.Received += (model, ea) =>
+                    {
+                        var data = ea.Body;
+                       
+                        var message = Encoding.UTF8.GetString(data);
+                        logger.Debug("Get messagge from ea.Body {message}", message);
+                        System.Console.WriteLine(message);
+                    };
+
+                    channel.BasicConsume("first", true, consumer);
+                    System.Console.WriteLine("Type [enter] to exit");
+                    System.Console.ReadLine();
+                    ;
+                }
+            }
+       
+        }
+
+
+    }
+}
+
---
+++ Send/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using RabbitMQ.Client;
+using Serilog;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.ColoredConsole()
+                .WriteTo.RollingFile(@"E:\C#\RabbitMQ\Send\Logs\Log-{Date}.txt")
+                .CreateLogger();
+
+            logger.Debug("The Send.cs was started");
+            
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            logger.Debug("Created new Connectionfactory with name= {HostName}", factory.HostName);
+            using (var connection = factory.CreateConnection())
+            {
+                logger.Debug("Created new connection {connection}", connection.ToString());
+                using(var channel = connection.CreateModel())
+                {
+                    string queueName = "first";
+                    channel.QueueDeclare(
+                        queue: queueName,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    logger.Debug("Created queye {name} ", queueName);
+
+                    string message = "Hello RabbitMQ!!";
+                    var data = Encoding.UTF8.GetBytes(message);
+                    logger.Debug("Encoded message: {message} to bytes", message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "first",
+                        basicProperties: null,
+                        body: data);
+                    logger.Information("Published data");
+                }
+                Console.WriteLine("Press Enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
